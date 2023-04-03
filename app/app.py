947406b1582b17fa3c82354a8749a311c7246155
kykhoo <kykhoo@YAHOO.COM>
2023-04-03 23:25:39 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,20 +11,21 @@ def hello_world():
     request_type_str = request.method
     if request_type_str == 'GET':
         return render_template('index.html', href2='')
+    
+    elseif str(studentgender) =='' or str(studentiq) =='' or str(studentparentincome) =='' or str(studentparentencouragement) =='':
+        return render_template('index.html', href2='Please insert student information.')
+    
     else:
         studentgender = request.form['Gender']
         studentiq = request.form['IQ']
         studentparentincome = request.form['ParentIncome']
         studentparentencouragement = request.form['ParentEncouragement']
 
-        if str(studentgender) =='' or str(studentiq) =='' or str(studentparentincome) =='' or str(studentparentencouragement) =='':
-            return render_template('index.html', href2='Please insert student information.')
-        else:
-            model = load('app/pre.joblib')
-            np_arr = np.array([studentgender, studentiq, studentparentincome, studentparentencouragement])
-            predictions = model.predict([np_arr])  
-            predictions_to_str = str(predictions)
-            #return predictions_to_str
-            return render_template('index.html', href2='The probability of this student (gender:'+str(studentgender)+', IQ:'+str(studentiq)+', parentincome:'+str(studentparentincome)+', parentencouragement:'+str(studentparentencouragement)+') going to university is: '+predictions_to_str())
+        model = load('app/pre.joblib')
+        np_arr = np.array([studentgender, studentiq, studentparentincome, studentparentencouragement])
+        predictions = model.predict([np_arr])  
+        predictions_to_str = str(predictions)
+        #return predictions_to_str
+        return render_template('index.html', href2='The probability of this student (gender:'+str(studentgender)+', IQ:'+str(studentiq)+', parentincome:'+str(studentparentincome)+', parentencouragement:'+str(studentparentencouragement)+') going to university is: '+predictions_to_str())
 
 
